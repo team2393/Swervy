bd81911b7ca4564d1a3fd0d1828b14c838e30301
Kay <kay.kasemir@gmail.com>
2022-04-16 13:16:53 -0400

    auto

---
+++ src/main/java/frc/robot/drivetrain/ResetCommand.java
@@ -0,0 +1,23 @@
+// Copyright (c) FIRST Team 2393 and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.drivetrain;
+
+import edu.wpi.first.wpilibj2.command.InstantCommand;
+
+/** Reset drivetrain position */
+public class ResetCommand extends InstantCommand
+{
+    public ResetCommand(final Drivetrain drivetrain)
+    {
+        super(drivetrain::reset);
+        setName("Reset");
+    }
+
+    @Override
+    public boolean runsWhenDisabled()
+    {
+        return true;
+    }
+}
